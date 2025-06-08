fd21855f28c495b326c6afff14041756af4f7782
Lenz_Win_Fractal <lorenz.rettenwender@stud-fh.campuswien.ac.at>
2025-06-08 22:52:25 +0200

    added packer_install_for_win

---
+++ packer_install_for_win.lua
@@ -0,0 +1,12 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  print("Installing packer.nvim...")
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+  print("packer.nvim installed!")
+else
+  print("packer.nvim already installed")
+end
+
